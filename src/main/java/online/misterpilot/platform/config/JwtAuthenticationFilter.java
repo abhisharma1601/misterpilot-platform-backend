e21288bbdbe0cac4cd7edde652e4065ae93ae9ed
Abhishek Sharma <4abhi45@gmail.com>
2026-09-07 00:00:00 +0530

    refactor: remove CostCalculatorService and its tests

--- src/main/java/online/misterpilot/platform/config/JwtAuthenticationFilter.java
+++ src/main/java/online/misterpilot/platform/config/JwtAuthenticationFilter.java
@@ -41,9 +41,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
 
-        String header = request.getHeader("Authorization");
-
-        
+        String header = request.getHeader("Authorization");        
 
         if (header != null && header.startsWith("Bearer ")) {
             String token = header.substring(7);
